4272441ea278ebecb2519fde8c152e9e08c899b7
Krzysztof Bartkowiak <bartkowiakkrzysiek000@gmail.com>
2024-06-03 21:32:37 +0200

    response to simple question with some handling

--- p_p.py
+++ p_p.py
@@ -1,5 +1,26 @@
 import openai
+from openai import OpenAI
 import os
 
-client = openai._os.environ.get('OPENAI_API_KEY')
+client = OpenAI(
+    api_key = os.environ.get("OPENAI_API_KEY")
+)
 
+messages = []
+messages.append({"role": "user", "content": "what programming language is the best?"})
+
+try:
+    response = client.chat.completions.create(messages = messages, 
+                                            model = "gpt-3.5-turbo",
+                                            n = 3,
+                                            temperature = 0.9,
+                                            max_tokens = 500,
+                                            )
+
+    print(response.choices[0].message.content)
+
+except openai.AuthenticationError:
+    print('no valid token / authentication error')
+except openai.BadRequestError as e:
+    print('invalid request')
+    print(e)
